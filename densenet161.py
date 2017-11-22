f62dc66f5162f6778bc1e2e9817ee164348dd7d7
chuang <358114866@qq.com>
2017-11-22 15:50:54 +0800

    Update densenet161.py

--- densenet161.py
+++ densenet161.py
@@ -12,13 +12,13 @@ def DenseNet(nb_dense_block=4, growth_rate=48, nb_filter=96, reduction=0.0, drop
     '''Instantiate the DenseNet 161 architecture,
         # Arguments
             nb_dense_block: number of dense blocks to add to end
-            growth_rate: number of filters to add per dense block
-            nb_filter: initial number of filters
-            reduction: reduction factor of transition blocks.
+            growth_rate: number of filters to add per dense block每个dense block后增加的滤波器数
+            nb_filter: initial number of filters初始数量的滤波器个数
+            reduction: reduction factor of transition blocks.过渡块的缩减因子。
             dropout_rate: dropout rate
-            weight_decay: weight decay factor
+            weight_decay: weight decay factor权重衰减因子
             classes: optional number of classes to classify images
-            weights_path: path to pre-trained weights
+            weights_path: path to pre-trained weights预训练权重路径
         # Returns
             A Keras model instance.
     '''
@@ -27,7 +27,7 @@ def DenseNet(nb_dense_block=4, growth_rate=48, nb_filter=96, reduction=0.0, drop
     # compute compression factor
     compression = 1.0 - reduction
 
-    # Handle Dimension Ordering for different backends
+    # Handle Dimension Ordering for different backends处理不同后端的维度排序
     global concat_axis
     if K.image_dim_ordering() == 'tf':
       concat_axis = 3
@@ -40,18 +40,18 @@ def DenseNet(nb_dense_block=4, growth_rate=48, nb_filter=96, reduction=0.0, drop
     nb_filter = 96
     nb_layers = [6,12,36,24] # For DenseNet-161
 
-    # Initial convolution
+    # Initial convolution初始化卷积
     x = ZeroPadding2D((3, 3), name='conv1_zeropadding')(img_input)
     x = Convolution2D(nb_filter, 7, 7, subsample=(2, 2), name='conv1', bias=False)(x)
-    x = BatchNormalization(epsilon=eps, axis=concat_axis, name='conv1_bn')(x)
+    x = BatchNormalization(epsilon=eps, axis=concat_axis, name='conv1_bn')(x)#eps 是距离？？
     x = Scale(axis=concat_axis, name='conv1_scale')(x)
     x = Activation('relu', name='relu1')(x)
     x = ZeroPadding2D((1, 1), name='pool1_zeropadding')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2), name='pool1')(x)
 
-    # Add dense blocks
-    for block_idx in range(nb_dense_block - 1):
-        stage = block_idx+2
+    # Add dense blocks  #增加dense block
+    for block_idx in range(nb_dense_block - 1):#012
+        stage = block_idx+2  #234
         x, nb_filter = dense_block(x, stage, nb_layers[block_idx], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
 
         # Add transition_block
@@ -72,55 +72,56 @@ def DenseNet(nb_dense_block=4, growth_rate=48, nb_filter=96, reduction=0.0, drop
     model = Model(img_input, x, name='densenet')
 
     if weights_path is not None:
-      model.load_weights(weights_path)
+      model.load_weights(weights_path)#下载权重
 
     return model
 
 
-def conv_block(x, stage, branch, nb_filter, dropout_rate=None, weight_decay=1e-4):
-    '''Apply BatchNorm, Relu, bottleneck 1x1 Conv2D, 3x3 Conv2D, and option dropout
+def conv_block(x, stage, branch, nb_filter, dropout_rate=None, weight_decay=1e-4):#dropout_rate=None
+    '''Apply BatchNorm, Relu, bottleneck 1x1 Conv2D, 3x3 Conv2D, and option dropout可选择dropout
         # Arguments
-            x: input tensor 
-            stage: index for dense block
-            branch: layer index within each dense block
+            x: input tensor #输入的是张量
+            stage: index for dense block 
+            branch: layer index within each dense block：dense块的层序号 
             nb_filter: number of filters
             dropout_rate: dropout rate
             weight_decay: weight decay factor
+            瓶颈层，目标是减少数据量
     '''
     eps = 1.1e-5
     conv_name_base = 'conv' + str(stage) + '_' + str(branch)
     relu_name_base = 'relu' + str(stage) + '_' + str(branch)
 
     # 1x1 Convolution (Bottleneck layer)
-    inter_channel = nb_filter * 4  
+    inter_channel = nb_filter * 4  #？？？
     x = BatchNormalization(epsilon=eps, axis=concat_axis, name=conv_name_base+'_x1_bn')(x)
     x = Scale(axis=concat_axis, name=conv_name_base+'_x1_scale')(x)
     x = Activation('relu', name=relu_name_base+'_x1')(x)
-    x = Convolution2D(inter_channel, 1, 1, name=conv_name_base+'_x1', bias=False)(x)
+    x = Convolution2D(inter_channel, 1, 1, name=conv_name_base+'_x1', bias=False)(x)#卷积核大小为1×1
 
-    if dropout_rate:
+    if dropout_rate:#如果有dropout—rate就用，但此处为0，代表无
         x = Dropout(dropout_rate)(x)
 
     # 3x3 Convolution
     x = BatchNormalization(epsilon=eps, axis=concat_axis, name=conv_name_base+'_x2_bn')(x)
     x = Scale(axis=concat_axis, name=conv_name_base+'_x2_scale')(x)
     x = Activation('relu', name=relu_name_base+'_x2')(x)
-    x = ZeroPadding2D((1, 1), name=conv_name_base+'_x2_zeropadding')(x)
-    x = Convolution2D(nb_filter, 3, 3, name=conv_name_base+'_x2', bias=False)(x)
+    x = ZeroPadding2D((1, 1), name=conv_name_base+'_x2_zeropadding')(x)#此处有Zero池化
+    x = Convolution2D(nb_filter, 3, 3, name=conv_name_base+'_x2', bias=False)(x)#后卷积
 
     if dropout_rate:
         x = Dropout(dropout_rate)(x)
 
     return x
 
-
+    #转化层
 def transition_block(x, stage, nb_filter, compression=1.0, dropout_rate=None, weight_decay=1E-4):
-    ''' Apply BatchNorm, 1x1 Convolution, averagePooling, optional compression, dropout 
+    ''' Apply BatchNorm, 1x1 Convolution, averagePooling, optional compression, dropout 卷积用1×1，avepool，可选择压缩和dropout
         # Arguments
             x: input tensor
             stage: index for dense block
             nb_filter: number of filters
-            compression: calculated as 1 - reduction. Reduces the number of feature maps in the transition block.
+            compression: calculated as 1 - reduction. Reduces the number of feature maps in the transition block.压缩用于在transition块减少特征图数量
             dropout_rate: dropout rate
             weight_decay: weight decay factor
     '''
@@ -145,21 +146,21 @@ def transition_block(x, stage, nb_filter, compression=1.0, dropout_rate=None, we
 
 def dense_block(x, stage, nb_layers, nb_filter, growth_rate, dropout_rate=None, weight_decay=1e-4, grow_nb_filters=True):
     ''' Build a dense_block where the output of each conv_block is fed to subsequent ones
-        # Arguments
+        # Arguments构建一个dense_block，其中每个conv_block的输出被馈送到后续的输出
             x: input tensor
             stage: index for dense block
-            nb_layers: the number of layers of conv_block to append to the model.
+            nb_layers: the number of layers of conv_block to append to the model.卷积块层数应用到这个模型
             nb_filter: number of filters
             growth_rate: growth rate
             dropout_rate: dropout rate
             weight_decay: weight decay factor
-            grow_nb_filters: flag to decide to allow number of filters to grow
+            grow_nb_filters: flag to decide to allow number of filters to grow标志来决定允许过滤器的数量增长
     '''
 
     eps = 1.1e-5
     concat_feat = x
 
-    for i in range(nb_layers):
+    for i in range(nb_layers):#dense块的层数
         branch = i+1
         x = conv_block(concat_feat, stage, branch, growth_rate, dropout_rate, weight_decay)
         concat_feat = merge([concat_feat, x], mode='concat', concat_axis=concat_axis, name='concat_'+str(stage)+'_'+str(branch))
