cb76f1e6f70f372731607108a634fac3a3d7b09f
chuang <358114866@qq.com>
2017-11-22 16:40:23 +0800

    Update test_inference.py

--- test_inference.py
+++ test_inference.py
@@ -5,27 +5,28 @@ import numpy as np
 from keras.optimizers import SGD
 import keras.backend as K
 
-# We only test DenseNet-121 in this script for demo purpose
+# We only test DenseNet-121 in this script for demo purpose 我们只在densenet121上运行了
 from densenet121 import DenseNet 
 
 im = cv2.resize(cv2.imread('resources/cat.jpg'), (224, 224)).astype(np.float32)
 #im = cv2.resize(cv2.imread('shark.jpg'), (224, 224)).astype(np.float32)
 
-# Subtract mean pixel and multiple by scaling constant 
-# Reference: https://github.com/shicai/DenseNet-Caffe
+# Subtract mean pixel and multiple by scaling constant 通过缩放常量减去平均像素和倍数
+# Reference: https://github.com/shicai/DenseNet-Caffe 
+#下面什么意思？？？
 im[:,:,0] = (im[:,:,0] - 103.94) * 0.017
 im[:,:,1] = (im[:,:,1] - 116.78) * 0.017
 im[:,:,2] = (im[:,:,2] - 123.68) * 0.017
 
 if K.image_dim_ordering() == 'th':
-  # Transpose image dimensions (Theano uses the channels as the 1st dimension)
+  # Transpose image dimensions (Theano uses the channels as the 1st dimension)移调图像尺寸（Theano使用通道作为第一维）
   im = im.transpose((2,0,1))
 
   # Use pre-trained weights for Theano backend
   weights_path = 'imagenet_models/densenet121_weights_th.h5'
 else:
   # Use pre-trained weights for Tensorflow backend
-  weights_path = 'imagenet_models/densenet121_weights_tf.h5'
+  weights_path = 'imagenet_models/densenet161_weights_tf.h5'#将121改成了densenet161
 
 # Insert a new dimension for the batch_size
 im = np.expand_dims(im, axis=0)
